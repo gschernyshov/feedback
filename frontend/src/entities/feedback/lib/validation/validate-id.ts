a5f1821e57c6ab85e6b26639708a013e78844218
gschernyshov <grchafv@yandex.ru>
2026-02-13 13:11:05 +0300

    fix(frontend, validation): обновлена валидация UUID для корректной обработки пробелов

--- frontend/src/entities/feedback/lib/validation/validate-id.ts
+++ frontend/src/entities/feedback/lib/validation/validate-id.ts
@@ -10,7 +10,7 @@ export const validationId = (id: string) => {
   }
 
   if (
-    !/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(id)
+    !/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(id)
   ) {
     return 'Id должен быть в формате UUID'
   }
