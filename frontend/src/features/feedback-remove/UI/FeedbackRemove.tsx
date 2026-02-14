c6e4004051321633538c91fc094a3cdea7f6e4a2
gschernyshov <grchafv@yandex.ru>
2026-02-14 20:28:58 +0300

    fix(frontend, features/feedback-remove): устранение багов вывода сообщений об удалении Feedback

--- frontend/src/features/feedback-remove/UI/FeedbackRemove.tsx
+++ frontend/src/features/feedback-remove/UI/FeedbackRemove.tsx
@@ -1,9 +1,5 @@
-import { useEffect } from 'react'
 import { useRemoveFeedbackMutation } from '../api/useRemoveFeedbackMutation'
-import { useAppDispatch } from '@/app/providers/store'
-import { addTemporary } from '@/features/notifications/model'
 import { type RemoveFeedback } from '@/entities/feedback/model'
-import { getErrorMessage } from '@/shared/lib/errors'
 import { Button } from '@/shared/UI/Form'
 
 interface FeedbackRemoveProps {
@@ -11,49 +7,15 @@ interface FeedbackRemoveProps {
 }
 
 export const FeedbackRemove = ({ id }: FeedbackRemoveProps) => {
-  const dispatch = useAppDispatch()
-  const [removeFeedback, { isLoading, isSuccess, data, isError, error }] =
-    useRemoveFeedbackMutation()
-
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(
-        addTemporary({
-          type: 'success',
-          message: `Feedback с id: ${id} успешно удалён`,
-        }),
-      )
-    }
-  }, [id, isSuccess, data, dispatch])
-
-  useEffect(() => {
-    if (isError) {
-      dispatch(
-        addTemporary({
-          type: 'error',
-          message: `При удалении Feedback c id: ${id} возникла ошибка`,
-          errorMessage: getErrorMessage(error),
-        }),
-      )
-    }
-  }, [id, isError, error, dispatch])
+  const [removeFeedback, { isLoading }] = useRemoveFeedbackMutation()
 
   const handleRemove = () => {
     removeFeedback(id)
   }
 
-  if (isSuccess) return null
-
   return (
-    <>
-      <Button
-        size="sm"
-        type="button"
-        disabled={isLoading}
-        onClick={handleRemove}
-      >
-        {isLoading ? 'Удаляем...' : 'Удалить'}
-      </Button>
-    </>
+    <Button size="sm" type="button" disabled={isLoading} onClick={handleRemove}>
+      {isLoading ? 'Удаляем...' : 'Удалить'}
+    </Button>
   )
 }
--- frontend/src/features/feedback-remove/api/useRemoveFeedbackMutation.ts
+++ frontend/src/features/feedback-remove/api/useRemoveFeedbackMutation.ts
@@ -1,9 +1,11 @@
+import { addTemporary } from '@/features/notifications/model'
 import {
   markAsDeleted,
   unmarkAsDeleted,
   type RemoveFeedback,
 } from '@/entities/feedback/model'
 import { baseApi } from '@/shared/api/baseApi'
+import { getErrorMessage } from '@/shared/lib/errors'
 
 const feedbackApi = baseApi.injectEndpoints({
   endpoints: builder => ({
@@ -16,9 +18,21 @@ const feedbackApi = baseApi.injectEndpoints({
         dispatch(markAsDeleted(id))
         try {
           await queryFulfilled
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        } catch (_) {
+          dispatch(
+            addTemporary({
+              type: 'success',
+              message: `Feedback с id: ${id} успешно удалён`,
+            }),
+          )
+        } catch (error) {
           dispatch(unmarkAsDeleted(id))
+          dispatch(
+            addTemporary({
+              type: 'error',
+              message: `При удалении Feedback c id: ${id} возникла ошибка`,
+              errorMessage: getErrorMessage(error),
+            }),
+          )
         }
       },
       invalidatesTags: [{ type: 'Feedback', id: 'LIST' }],
